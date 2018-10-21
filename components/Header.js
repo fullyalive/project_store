9e56cdcc43bb06ee2f0923609b94a4ac281336ad
fullyalive <wogud1202@gmail.com>
2018-10-21 14:17:56 +0900

    Styles in Next.js

--- components/Header.js
+++ components/Header.js
@@ -16,5 +16,32 @@ export default () => (
         </li>
       </ul>
     </nav>
+    <style jsx>
+      {`
+        header {
+          padding: 10px;
+          border-bottom: 1px solid #3fa9f5;
+        }
+
+        header a {
+          color: #3fa9f5;
+          font-size: 20px;
+          font-weight: 700;
+        }
+
+        a {
+          text-decoration: none;
+          color: black;
+        }
+
+        ul {
+          display: flex;
+          list-style: none;
+        }
+        ul li {
+          margin-right: 20px;
+        }
+      `}
+    </style>
   </header>
 );
--- components/PostLink.js
+++ components/PostLink.js
@@ -3,6 +3,15 @@ import Link from "next/link";
 export default props => (
   <Link href={`/post?title=${props.title}`} as={`/post/${props.title}`}>
     {/* Route masking 하는 것임 - 사용자들에게 URL을 더 깔끔하게 보이게끔.*/}
-    <a>{props.title}</a>
+    <a>
+      {props.title}
+      <style jsx global>
+        {`
+          body {
+            background-color: white;
+          }
+        `}
+      </style>
+    </a>
   </Link>
 );
