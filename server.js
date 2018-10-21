9118c85ec0859a2ba9517ee18cbaeb0b6c4de161
fullyalive <wogud1202@gmail.com>
2018-10-21 14:00:49 +0900

    Pretty URLS - 2

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app
   .prepare()
   .then(() => {
     const server = express();
+
+    server.get("/post/:title", (req, res) => {
+      const actualPage = "/post";
+      const queryParams = { title: req.params.title }; // queryParameter는 같아야 한다. 13 title과  15 title 이 두개가 맞아야 됨
+      app.render(req, res, actualPage, queryParams);
+    }); // router masking으로 사용자에게 보여지는 url이 그대로 서버에 요청될때, 보여주는 페이지를 /post가 되도록
+
     server.get("*", (req, res) => {
       return handle(req, res);
     });
