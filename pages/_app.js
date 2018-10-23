876dc0cdfec675fe26203c466b690237633d4c42
fullyalive <wogud1202@gmail.com>
2018-10-24 00:45:46 +0900

    getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,20 @@ import Header from "../components/Header";
 import React from "react";
 
 export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    // component가 render되기 전에 불리는 함수 - data가 있을 때만 component를 render
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
     return (
       <Container>
         <Header />
-        <Component />
+        <Component {...pageProps} />
       </Container>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
 import Head from "next/head";
-import withLayout from "../lib/withLayout";
 import PostLink from "../components/PostLink";
 
-export default () => (
-  <div>
-    <Head>
-      <title>Home | Fullyalive Store</title>
-    </Head>
-    <h1>Movies</h1>
-    <ul>
-      <li>
-        <PostLink title={"Something"} id={0} />
-      </li>
-      <li>
-        <PostLink title={"Something else"} id={1} />
-      </li>
-    </ul>
-  </div>
-);
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Home | Fullyalive Store</title>
+        </Head>
+        <h1>Movies</h1>
+        <ul>
+          <li>
+            <PostLink title={"Something"} id={0} />
+          </li>
+          <li>
+            <PostLink title={"Something else"} id={1} />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Index;
